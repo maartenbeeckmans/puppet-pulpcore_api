c14edb873e5fa6253538609ee1077680512bb14f
Dieter Maes <dmaes@inuits.eu>
2024-09-20 12:13:45 +0200

    rubocop fixes

--- lib/puppet_x/pulpcore_api/config.rb
+++ lib/puppet_x/pulpcore_api/config.rb
@@ -3,61 +3,63 @@
 require 'yaml'
 require 'puppet'
 
-class PuppetX::PulpcoreApi::Config # rubocop:disable Style/Documentation
-  CONFIG_PULPCORE_SCHEME = :scheme
-  CONFIG_PULPCORE_HOST = :host
-  CONFIG_PULPCORE_SSL_VERIFY = :ssl_verify
-  CONFIG_PULPCORE_USERNAME = :username
-  CONFIG_PULPCORE_PASSWORD = :password
+module PuppetX::PulpcoreApi # rubocop:disable Style/ClassAndModuleChildren
+  class Config # rubocop:disable Style/Documentation
+    CONFIG_PULPCORE_SCHEME = :scheme
+    CONFIG_PULPCORE_HOST = :host
+    CONFIG_PULPCORE_SSL_VERIFY = :ssl_verify
+    CONFIG_PULPCORE_USERNAME = :username
+    CONFIG_PULPCORE_PASSWORD = :password
 
-  def self.configure
-    @config ||= read_config
-  end
-
-  def self.file_path
-    @config_file_path ||= File.expand_path(File.join(Puppet.settings[:confdir], '/pulpcoreapi.yaml'))
-  end
-
-  def self.reset
-    @config = nil
-    @config_file_path = nil
-  end
-
-  def self.read_config
-    begin
-      Puppet.debug("Parsing configuration file #{file_path}")
-      config = {}
-      # Ugly hack to convert hash keys from String to Symbol
-      YAML.load_file(file_path).each { |key, value| config[key.to_sym] = value }
-    rescue => e
-      raise Puppet::ParseError, "Could not parse YAML configuration file '#{file_path}': #{e}"
+    def self.configure
+      @config ||= read_config
     end
 
-    if config[CONFIG_PULPCORE_SCHEME].nil?
-      raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_SCHEME}'"
+    def self.file_path
+      @config_file_path ||= File.expand_path(File.join(Puppet.settings[:confdir], '/pulpcoreapi.yaml'))
     end
 
-    if config[CONFIG_PULPCORE_HOST].nil?
-      raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_HOST}'"
+    def self.reset
+      @config = nil
+      @config_file_path = nil
     end
 
-    if config[CONFIG_PULPCORE_SCHEME] == 'http' && config[CONFIG_PULPCORE_HOST] != 'localhost'
+    def self.read_config
+      begin
+        Puppet.debug("Parsing configuration file #{file_path}")
+        config = {}
+        # Ugly hack to convert hash keys from String to Symbol
+        YAML.load_file(file_path).each { |key, value| config[key.to_sym] = value }
+      rescue => e
+        raise Puppet::ParseError, "Could not parse YAML configuration file '#{file_path}': #{e}"
+      end
+
+      if config[CONFIG_PULPCORE_SCHEME].nil?
+        raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_SCHEME}'"
+      end
+
+      if config[CONFIG_PULPCORE_HOST].nil?
+        raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_HOST}'"
+      end
+
+      if config[CONFIG_PULPCORE_SCHEME] == 'http' && config[CONFIG_PULPCORE_HOST] != 'localhost'
+        Puppet.warning 'insecure connection, sending credentials over http'
+      end
+
+      if config[CONFIG_PULPCORE_SSL_VERIFY].nil?
+        raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_SSL_VERIFY}'"
+      end
+
+      if config[CONFIG_PULPCORE_USERNAME].nil?
+        raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_USERNAME}'"
+      end
+
+      if config[CONFIG_PULPCORE_PASSWORD].nil?
+        raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_PASSWORD}'"
+      end
+
       Puppet.warning 'insecure connection, sending credentials over http'
+      config
     end
-
-    if config[CONFIG_PULPCORE_SSL_VERIFY].nil?
-      raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_SSL_VERIFY}'"
-    end
-
-    if config[CONFIG_PULPCORE_USERNAME].nil?
-      raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_USERNAME}'"
-    end
-
-    if config[CONFIG_PULPCORE_PASSWORD].nil?
-      raise Puppet::ParseError, "Config file #{file_path} must contain a value for key '#{CONFIG_PULPCORE_PASSWORD}'"
-    end
-
-    Puppet.warning 'insecure connection, sending credentials over http'
-    config
   end
 end
--- lib/puppet_x/pulpcore_api/helper_functions.rb
+++ lib/puppet_x/pulpcore_api/helper_functions.rb
@@ -28,26 +28,14 @@ rescue LoadError
   Puppet.warning("#{__FILE__}:#{__LINE__}: pulp_rpm_client gem was not found")
 end
 
-class PuppetX::PulpcoreApi::HelperFunctions # rubocop:disable Style/Documentation
-  def self.get_api_instance(plugin, object)
-    apiconfig = PuppetX::PulpcoreApi::Config.configure
+module PuppetX::PulpcoreApi # rubocop:disable Style/ClassAndModuleChildren
+  class HelperFunctions # rubocop:disable Style/Documentation
+    def self.get_api_instance(plugin, object)
+      apiconfig = PuppetX::PulpcoreApi::Config.configure
 
-    case plugin
-    when 'container'
-      PulpContainerClient.configure do |config|
-        config.scheme          = apiconfig[:scheme]
-        config.host            = apiconfig[:host]
-        config.ssl_verify      = apiconfig[:ssl_verify]
-        config.ssl_ca_file     = apiconfig[:ssl_ca_file]
-        config.ssl_client_cert = apiconfig[:ssl_client_cert]
-        config.ssl_client_key  = apiconfig[:ssl_client_key]
-        config.username        = apiconfig[:username]
-        config.password        = apiconfig[:password]
-      end
-
-      case object
-      when 'content_guard'
-        PulpcoreClient.configure do |config|
+      case plugin
+      when 'container'
+        PulpContainerClient.configure do |config|
           config.scheme          = apiconfig[:scheme]
           config.host            = apiconfig[:host]
           config.ssl_verify      = apiconfig[:ssl_verify]
@@ -57,31 +45,31 @@ class PuppetX::PulpcoreApi::HelperFunctions # rubocop:disable Style/Documentatio
           config.username        = apiconfig[:username]
           config.password        = apiconfig[:password]
         end
-        api_instance = PulpcoreClient::ContentguardsApi.new
-      when 'distribution'
-        api_instance = PulpContainerClient::DistributionsContainerApi.new
-      when 'remote'
-        api_instance = PulpContainerClient::RemotesContainerApi.new
-      when 'repository'
-        api_instance = PulpContainerClient::RepositoriesContainerApi.new
-      else
-        raise Puppet::ParseError, "Object #{object} not supported for plugin #{plugin}"
-      end
-    when 'deb'
-      PulpDebClient.configure do |config|
-        config.scheme          = apiconfig[:scheme]
-        config.host            = apiconfig[:host]
-        config.ssl_verify      = apiconfig[:ssl_verify]
-        config.ssl_ca_file     = apiconfig[:ssl_ca_file]
-        config.ssl_client_cert = apiconfig[:ssl_client_cert]
-        config.ssl_client_key  = apiconfig[:ssl_client_key]
-        config.username        = apiconfig[:username]
-        config.password        = apiconfig[:password]
-      end
 
-      case object
-      when 'content_guard'
-        PulpcoreClient.configure do |config|
+        case object
+        when 'content_guard'
+          PulpcoreClient.configure do |config|
+            config.scheme          = apiconfig[:scheme]
+            config.host            = apiconfig[:host]
+            config.ssl_verify      = apiconfig[:ssl_verify]
+            config.ssl_ca_file     = apiconfig[:ssl_ca_file]
+            config.ssl_client_cert = apiconfig[:ssl_client_cert]
+            config.ssl_client_key  = apiconfig[:ssl_client_key]
+            config.username        = apiconfig[:username]
+            config.password        = apiconfig[:password]
+          end
+          api_instance = PulpcoreClient::ContentguardsApi.new
+        when 'distribution'
+          api_instance = PulpContainerClient::DistributionsContainerApi.new
+        when 'remote'
+          api_instance = PulpContainerClient::RemotesContainerApi.new
+        when 'repository'
+          api_instance = PulpContainerClient::RepositoriesContainerApi.new
+        else
+          raise Puppet::ParseError, "Object #{object} not supported for plugin #{plugin}"
+        end
+      when 'deb'
+        PulpDebClient.configure do |config|
           config.scheme          = apiconfig[:scheme]
           config.host            = apiconfig[:host]
           config.ssl_verify      = apiconfig[:ssl_verify]
@@ -91,33 +79,33 @@ class PuppetX::PulpcoreApi::HelperFunctions # rubocop:disable Style/Documentatio
           config.username        = apiconfig[:username]
           config.password        = apiconfig[:password]
         end
-        api_instance = PulpcoreClient::ContentguardsApi.new
-      when 'distribution'
-        api_instance = PulpDebClient::DistributionsAptApi.new
-      when 'remote'
-        api_instance = PulpDebClient::RemotesAptApi.new
-      when 'repository'
-        api_instance = PulpDebClient::RepositoriesAptApi.new
-      when 'publication'
-        api_instance = PulpDebClient::PublicationsAptApi.new
-      else
-        raise Puppet::ParseError, "Object #{object} not supported for plugin #{plugin}"
-      end
-    when 'file'
-      PulpFileClient.configure do |config|
-        config.scheme          = apiconfig[:scheme]
-        config.host            = apiconfig[:host]
-        config.ssl_verify      = apiconfig[:ssl_verify]
-        config.ssl_ca_file     = apiconfig[:ssl_ca_file]
-        config.ssl_client_cert = apiconfig[:ssl_client_cert]
-        config.ssl_client_key  = apiconfig[:ssl_client_key]
-        config.username        = apiconfig[:username]
-        config.password        = apiconfig[:password]
-      end
 
-      case object
-      when 'content_guard'
-        PulpcoreClient.configure do |config|
+        case object
+        when 'content_guard'
+          PulpcoreClient.configure do |config|
+            config.scheme          = apiconfig[:scheme]
+            config.host            = apiconfig[:host]
+            config.ssl_verify      = apiconfig[:ssl_verify]
+            config.ssl_ca_file     = apiconfig[:ssl_ca_file]
+            config.ssl_client_cert = apiconfig[:ssl_client_cert]
+            config.ssl_client_key  = apiconfig[:ssl_client_key]
+            config.username        = apiconfig[:username]
+            config.password        = apiconfig[:password]
+          end
+          api_instance = PulpcoreClient::ContentguardsApi.new
+        when 'distribution'
+          api_instance = PulpDebClient::DistributionsAptApi.new
+        when 'remote'
+          api_instance = PulpDebClient::RemotesAptApi.new
+        when 'repository'
+          api_instance = PulpDebClient::RepositoriesAptApi.new
+        when 'publication'
+          api_instance = PulpDebClient::PublicationsAptApi.new
+        else
+          raise Puppet::ParseError, "Object #{object} not supported for plugin #{plugin}"
+        end
+      when 'file'
+        PulpFileClient.configure do |config|
           config.scheme          = apiconfig[:scheme]
           config.host            = apiconfig[:host]
           config.ssl_verify      = apiconfig[:ssl_verify]
@@ -127,33 +115,33 @@ class PuppetX::PulpcoreApi::HelperFunctions # rubocop:disable Style/Documentatio
           config.username        = apiconfig[:username]
           config.password        = apiconfig[:password]
         end
-        api_instance = PulpcoreClient::ContentguardsApi.new
-      when 'distribution'
-        api_instance = PulpFileClient::DistributionsFileApi.new
-      when 'remote'
-        api_instance = PulpFileClient::RemotesFileApi.new
-      when 'repository'
-        api_instance = PulpFileClient::RepositoriesFileApi.new
-      when 'publication'
-        api_instance = PulpFileClient::PublicationsFileApi.new
-      else
-        raise Puppet::ParseError, "Object #{object} not supported for plugin #{plugin}"
-      end
-    when 'rpm'
-      PulpRpmClient.configure do |config|
-        config.scheme          = apiconfig[:scheme]
-        config.host            = apiconfig[:host]
-        config.ssl_verify      = apiconfig[:ssl_verify]
-        config.ssl_ca_file     = apiconfig[:ssl_ca_file]
-        config.ssl_client_cert = apiconfig[:ssl_client_cert]
-        config.ssl_client_key  = apiconfig[:ssl_client_key]
-        config.username        = apiconfig[:username]
-        config.password        = apiconfig[:password]
-      end
 
-      case object
-      when 'content_guard'
-        PulpcoreClient.configure do |config|
+        case object
+        when 'content_guard'
+          PulpcoreClient.configure do |config|
+            config.scheme          = apiconfig[:scheme]
+            config.host            = apiconfig[:host]
+            config.ssl_verify      = apiconfig[:ssl_verify]
+            config.ssl_ca_file     = apiconfig[:ssl_ca_file]
+            config.ssl_client_cert = apiconfig[:ssl_client_cert]
+            config.ssl_client_key  = apiconfig[:ssl_client_key]
+            config.username        = apiconfig[:username]
+            config.password        = apiconfig[:password]
+          end
+          api_instance = PulpcoreClient::ContentguardsApi.new
+        when 'distribution'
+          api_instance = PulpFileClient::DistributionsFileApi.new
+        when 'remote'
+          api_instance = PulpFileClient::RemotesFileApi.new
+        when 'repository'
+          api_instance = PulpFileClient::RepositoriesFileApi.new
+        when 'publication'
+          api_instance = PulpFileClient::PublicationsFileApi.new
+        else
+          raise Puppet::ParseError, "Object #{object} not supported for plugin #{plugin}"
+        end
+      when 'rpm'
+        PulpRpmClient.configure do |config|
           config.scheme          = apiconfig[:scheme]
           config.host            = apiconfig[:host]
           config.ssl_verify      = apiconfig[:ssl_verify]
@@ -163,58 +151,72 @@ class PuppetX::PulpcoreApi::HelperFunctions # rubocop:disable Style/Documentatio
           config.username        = apiconfig[:username]
           config.password        = apiconfig[:password]
         end
-        api_instance = PulpcoreClient::ContentguardsApi.new
-      when 'distribution'
-        api_instance = PulpRpmClient::DistributionsRpmApi.new
-      when 'remote'
-        api_instance = PulpRpmClient::RemotesRpmApi.new
-      when 'repository'
-        api_instance = PulpRpmClient::RepositoriesRpmApi.new
-      when 'publication'
-        api_instance = PulpRpmClient::PublicationsRpmApi.new
+
+        case object
+        when 'content_guard'
+          PulpcoreClient.configure do |config|
+            config.scheme          = apiconfig[:scheme]
+            config.host            = apiconfig[:host]
+            config.ssl_verify      = apiconfig[:ssl_verify]
+            config.ssl_ca_file     = apiconfig[:ssl_ca_file]
+            config.ssl_client_cert = apiconfig[:ssl_client_cert]
+            config.ssl_client_key  = apiconfig[:ssl_client_key]
+            config.username        = apiconfig[:username]
+            config.password        = apiconfig[:password]
+          end
+          api_instance = PulpcoreClient::ContentguardsApi.new
+        when 'distribution'
+          api_instance = PulpRpmClient::DistributionsRpmApi.new
+        when 'remote'
+          api_instance = PulpRpmClient::RemotesRpmApi.new
+        when 'repository'
+          api_instance = PulpRpmClient::RepositoriesRpmApi.new
+        when 'publication'
+          api_instance = PulpRpmClient::PublicationsRpmApi.new
+        else
+          raise Puppet::ParseError, "Object #{object} not supported for plugin #{plugin}"
+        end
       else
-        raise Puppet::ParseError, "Object #{object} not supported for plugin #{plugin}"
+        raise Puppet::ParseError, "Plugin #{plugin} is not supported"
       end
-    else
-      raise Puppet::ParseError, "Plugin #{plugin} is not supported"
+
+      api_instance
     end
 
-    api_instance
-  end
+    def self.get_pulp_href(name, plugin, object)
+      api_instance = get_api_instance(plugin, object)
 
-  def self.get_pulp_href(name, plugin, object)
-    api_instance = get_api_instance(plugin, object)
+      # Content guards is currently not supported
+      # should be fixed later
+      if object == 'content_guard'
+        return nil
+      end
 
-    # Content guards is currently not supported
-    # should be fixed later
-    if object == 'content_guard'
-      return nil
+      begin
+        response = api_instance.list({ limit: 1, name: name }).to_hash
+        return nil if response[:count] == 0
+      rescue PulpRpmClient::ApiError => e
+        raise Puppet::ParseError, "Exception when calling #{api_instance}->list: #{e}"
+      end
+      response[:results][0][:pulp_href]
     end
 
-    begin
-      response = api_instance.list({ limit: 1, name: name }).to_hash
-      return nil if response[:count] == 0
-    rescue PulpRpmClient::ApiError => e
-      raise Puppet::ParseError, "Exception when calling #{api_instance}->list: #{e}"
-    end
-    response[:results][0][:pulp_href]
-  end
+    def self.get_namevar(pulp_href, plugin, object)
+      api_instance = get_api_instance(plugin, object)
 
-  def self.get_namevar(pulp_href, plugin, object)
-    api_instance = get_api_instance(plugin, object)
+      # Content guards is currently not supported
+      # should be fixed later
+      if object == 'content_guard'
+        return nil
+      end
 
-    # Content guards is currently not supported
-    # should be fixed later
-    if object == 'content_guard'
-      return nil
-    end
-
-    begin
-      response = get_api_instance(plugin, object).read(pulp_href).to_hash
-      return response[:name] unless response[:name].nil?
-      return response[:repository_version] unless response[:repository_version].nil?
-    rescue PulpRpmClient::ApiError => e
-      raise Puppet::ParseError, "Exception when calling #{api_instance}->read: #{e}"
+      begin
+        response = get_api_instance(plugin, object).read(pulp_href).to_hash
+        return response[:name] unless response[:name].nil?
+        return response[:repository_version] unless response[:repository_version].nil?
+      rescue PulpRpmClient::ApiError => e
+        raise Puppet::ParseError, "Exception when calling #{api_instance}->read: #{e}"
+      end
     end
   end
 end
